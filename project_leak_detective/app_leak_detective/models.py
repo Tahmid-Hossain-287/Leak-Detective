714a8abb2626109e591636a563525ba1760aa647
Tahmid-Hossain-287 <mstahmid0@gmail.com>
2024-11-17 00:32:39 -0600

    added database model WaterData

--- project_leak_detective/app_leak_detective/models.py
+++ project_leak_detective/app_leak_detective/models.py
@@ -10,9 +10,9 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
-Class WaterData(models.Model):
+class WaterData(models.Model):
 	distance = 	models.FloatField()
 	timestamp = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
-        return f"Distance: {self.distance} at {self.timestamp}"
+		return f"Distance: {self.distance} at {self.timestamp}"
