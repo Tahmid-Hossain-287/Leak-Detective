48fe509aec2d648d55904df79dbc825a40a3f557
Tahmid-Hossain-287 <mstahmid0@gmail.com>
2024-11-17 02:20:48 -0600

    changed name in urls.py

--- project_leak_detective/app_leak_detective/urls.py
+++ project_leak_detective/app_leak_detective/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import WaterLeakageDataView
 
 urlpatterns = [
-    path('api/leak-data/', WaterLeakageDataView.as_view(), name='leak_data999'),
+    path('api/leak-data/', WaterLeakageDataView.as_view(), name='leak_data'),
     # path('api/', WaterLeakageDataView.as_view()),
 ]
