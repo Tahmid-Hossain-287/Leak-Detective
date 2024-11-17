15383a561da59513efc2df51e92ab848393f0e71
Tahmid-Hossain-287 <mstahmid0@gmail.com>
2024-11-17 03:08:27 -0600

    cleared commented out code from views.py

--- project_leak_detective/app_leak_detective/views.py
+++ project_leak_detective/app_leak_detective/views.py
@@ -6,20 +6,6 @@ from .models import WaterData
 from .serializers import WaterDataSerializer
 from django.utils import timezone
 
-# class WaterLeakageDataView(APIView):
-#     def get(self, request):
-#         # Simulate sensor data
-#         leak_data = {
-#             'timestamp': int(time.time()),
-#             'pressure': round(random.uniform(0.5, 5.0), 2),
-#             'flow_rate': round(random.uniform(0.1, 2.0), 2),
-#             'leak_detected': random.choice([True, False]),
-#             'leak_severity': random.choice(['None', 'Low', 'Medium', 'High'])
-#         }
-#         return Response(leak_data)
-
-
-
 class WaterLeakageDataView(APIView):
     def get(self, request):
         # Simulate sensor data
