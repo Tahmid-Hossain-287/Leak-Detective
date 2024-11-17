9eb2c47909147cbaf5bc5b1430fc874401a400fa
Tahmid-Hossain-287 <mstahmid0@gmail.com>
2024-11-17 00:28:43 -0600

    added database model WaterData

--- project_leak_detective/app_leak_detective/models.py
+++ project_leak_detective/app_leak_detective/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
+from django.utils import timezone
 
-# Create your models here.
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
+
+Class WaterData(models.Model):
+	distance = 	models.FloatField()
+	timestamp = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+        return f"Distance: {self.distance} at {self.timestamp}"
